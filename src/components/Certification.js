f36ba6ead3c1cd72868860fb704a23dddbb64a3c
Pushpinder Bal <pushpinderbal98@gmail.com>
2022-01-06 15:32:42 -0500

    Modified CSS and Skills

--- src/components/Certification.js
+++ src/components/Certification.js
@@ -8,25 +8,25 @@ import imageRHCSA from '../assets/rhcsa.png'
 
 const Certification = () => {
     return (
-        <div>
-               <section className='certificate-section' id="skills">
-        <div className="col-md-12">
+      <div>
+        <section className='certificate-section' id="skills">
           <div className="col-md-12">
-            <h1 className="section-title">
-              <span className="text-white">Certifications</span>
-            </h1>
+            <div className="col-md-12">
+              <h1 className="section-title">
+                <span className="text-white">Certifications</span>
+              </h1>
+            </div>
+            <div className="col-md-12 text-center">
+              <ul className="list-inline mx-auto skill-icon certification">
+                  <img src={imageCCNA} alt="" />
+                  <img src={imageAWS} alt="" />
+                  <img src={imageArchitect} alt="" />
+                  <img src={imageRHCSA} alt="" />
+              </ul>
+            </div>
           </div>
-          <div className="col-md-12 text-center">
-            <ul className="list-inline mx-auto skill-icon certification">
-                <img src={imageCCNA} alt="" />
-                <img src={imageAWS} alt="" />
-                <img src={imageArchitect} alt="" />
-                <img src={imageRHCSA} alt="" />
-            </ul>
-          </div>
-        </div>
-      </section>
-        </div>
+        </section>
+      </div>
     )
 }
 

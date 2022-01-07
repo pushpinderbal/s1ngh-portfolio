a6d2d2cba1fc83fda60357f49a0cc289989eb23d
Pushpinder Bal <pushpinderbal98@gmail.com>
2022-01-06 21:06:41 -0500

    updated about

--- src/components/About.js
+++ src/components/About.js
@@ -79,7 +79,7 @@ class About extends Component {
                     }}
                   >
                     <br />
-                    <span className="wave">{hello} :) </span>
+                    <span className="wave">{hello} ☬</span>
                     <br />
                     <br />
                     {about}
